e97c4bade5389f53d91b3f07beae337a1b7febae
Sukirno <mblonyox@gmail.com>
2022-11-03 11:03:55 +0700

    fix: add ms teams connector

--- monitify/teams.py
+++ monitify/teams.py
@@ -1,4 +1,16 @@
 
-def send_webhook(message: str, url: str) -> None:
-  # TODO: implements send_webook
-  pass
+from pymsteams import connectorcard
+from queue import Queue
+from monitify.notification import BaseNotificationWorker
+
+
+class TeamsNotificationWorker(BaseNotificationWorker):
+    def __init__(self, url: str, queue: Queue) -> None:
+        super().__init__(queue)
+        self.url = url
+
+    def send_message(self, name: str, items: list[str]):
+        message = connectorcard(self.url)
+        message.title("❗ Pemberitahuan ❗")
+        message.text(
+            f"{len(items)} item baru pada {name}:\n\n - " + "\n - ".join(items))
--- monitify/wa.py
+++ monitify/wa.py
@@ -4,12 +4,12 @@ from monitify.notification import BaseNotificationWorker
 
 
 class WaNotificationWorker(BaseNotificationWorker):
-    def __init__(self, url: str, api_key: str, chat_id: str, queue: Queue, *args, **kwargs) -> None:
-        super().__init__(queue, *args, **kwargs)
+    def __init__(self, url: str, api_key: str, chat_id: str, queue: Queue) -> None:
+        super().__init__(queue=queue)
         self.client = SocketClient(url, api_key)
         self.chat_id = chat_id
         print("WaNotificationWorker initialized.")
 
     def send_message(self, name: str, items: list[str]):
         self.client.sendText(
-            self.chat_id, f"❗ Item baru pada {name}:\n🆕 " + "\n🆕 ".join(items))
+            self.chat_id, f"❗ *Pemberitahuan* ❗\n {len(items)} item baru pada {name}:\n\n🆕 " + "\n🆕 ".join(items))
