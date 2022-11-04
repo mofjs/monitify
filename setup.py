cada02eef25626fa82a590173cfb8509acbc80a4
Sukirno <mblonyox@gmail.com>
2022-11-04 18:04:04 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Monitify",
+    version="1.0.0",
+    author="Sukirno",
+    author_email="mblonyox@gmail.com",
+    description="Simple script to monitor and notify new email or file to WhatsApp chat or Teams channel.",
+    long_description=open("README.MD", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords=["monitoring", "email", "owncloud", "wa", "teams"],
+    license="MIT",
+    url="https://github.com/mofjs/monitify",
+    packages=find_packages(),
+    python_requires=">=3.9.0",
+    install_requires=["typer", "pyocclient", "pymsteams", "wa-automate-socket-client", "rich", "shellingham"],
+    entry_points={
+        "console_scripts": [
+            "monitify=monitify.cli:app"
+        ]
+    }
+)
